46f746b3f64a418f4634e4ff9cac2c40c79fa8fb
Pedro Sousa <ppls2106@gmail.com>
2021-10-27 00:49:51 -0300

    feat(stage4): adicao do JS

---
+++ index.js
@@ -0,0 +1,32 @@
+const linksSocialMedia = {
+    github: 'sousapedro11',
+    youtube: 'pedrofearless',
+    facebook: 'sousapedro11',
+    instagram: 'sousapedro11',
+    twitter: 'ppls2106',
+}
+
+function changeSocialMediaLinks() {
+    for (const li of socialLinks.children) {
+        const social = li.getAttribute('class')
+
+        li.children[0].href = `https://www.${social}.com/${linksSocialMedia[social]}`
+    }
+}
+
+function getGithubProfile() {
+    const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            userName.textContent = data.name
+            userImage.src = data.avatar_url
+            userBio.textContent = data.bio
+            userLink.href = data.html_url
+            userLogin.textContent = data.login
+        })
+}
+
+changeSocialMediaLinks()
+getGithubProfile()
